b289d280b8eedb5013d856bf9facf00f607fc19a
jvyden <jvyden@jvyden.xyz>
2025-09-24 00:58:13 -0400

    More tests, move fonts to common ExampleFonts project

--- SharpFont.Tests/FaceTests.cs
+++ SharpFont.Tests/FaceTests.cs
@@ -3,9 +3,13 @@
 public class FaceTests : SharpFontTest
 {
     [Test]
-    public void LoadsFace()
+    [TestCase("Cousine-Regular-Latin.ttf")]
+    [TestCase("AlexBrush-Regular.ttf")]
+    [TestCase("Alegreya-Regular.otf")]
+    public void LoadsFace(string font)
     {
-        using Face face = new(this.Library, "Cousine-Regular-Latin.ttf");
+        using Face face = new(this.Library, font);
         Assert.That(face.GlyphCount, Is.GreaterThan(0));
+        TestContext.Out.WriteLine($"Loaded {face.FamilyName} (style: {face.StyleName})");
     }
 }
